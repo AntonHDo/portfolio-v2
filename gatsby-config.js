46b7b5fcabbeb0ca7b2dd2a70b4258ed232c6331
anton <antonhdo@gmail.com>
2023-07-27 22:34:50 -0700

    Try again

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,16 @@ module.exports = {
     siteURL: `https://chrisvo.dev/`,
     linkedInURL: `https://www.linkedin.com/in/chris-vo-/`,
     githubURL: `https://github.com/VoChrisK`,
-    angellistURL: `https://angel.co/chris-vo-3`
+    angellistURL: `https://angel.co/chris-vo-3`,
+    // Add any missing fields below
+    headline: "Your headline goes here",
+    basePath: "/",
+    version: "1.0",
+    developer: "Your name",
+    coauthorBusiness: "Coauthor's Business",
+    project: "Project name",
+    url: "https://example.com",
+    message: "Your message",
   },
   plugins: [
     `gatsby-plugin-scroll-reveal-with-new-react`,
